73a324a212cbbda20554245dea97acd53f7a619f
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-07-21 00:38:47 +0900

    nameでログインできるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+# デバイス機能実行前にconfigure_permitted_parametersを実行する。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+  	# 新規登録（sign_up）の際にnameデータの操作を許可する。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
 end
