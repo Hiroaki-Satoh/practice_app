2bce3cb9e67d57cb8c94e587f85b69c55ce057ad
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-07-23 00:05:39 +0900

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image, destroy: false
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: {maximum: 50}
 end
