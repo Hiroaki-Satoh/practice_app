9b8c20fc9c2a27698382fa80220be95371d3f5e2
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-07-29 22:05:40 +0900

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 
 end
