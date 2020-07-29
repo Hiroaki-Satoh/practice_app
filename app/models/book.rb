d7c2874b6b3ef6a24351e68f7d823ca180e794c4
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-07-29 22:06:02 +0900

    modified book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  attachment :book_image, destroy: false
+
+  belongs_to :user
+
+  # バリデーション
+  validates :title, presence: true
+  validates :body, presence: true, length: {maximum: 200}
 end
