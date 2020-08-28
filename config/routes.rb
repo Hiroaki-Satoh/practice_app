3779b0457d5dc4d3abedaf32fc1a6625b4dfcf17
Hiroaki-Satoh <yurayuraclover.liebe@gmail.com>
2020-08-28 16:27:53 +0900

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
   	# 投稿へのいいね機能
-  	resources :favorites,     only: [:create, :destroy]
+  	resource :favorites,     only: [:create, :destroy]
   	# 投稿へのコメント機能
-  	resources :post_commnets, only: [:create, :destroy]
+  	resource :post_commnets, only: [:create, :destroy]
   end
 end
